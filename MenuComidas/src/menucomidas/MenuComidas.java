58dc12210b0976bcc92b8048c83940c4543f6fe7
jesus <jesus@RobinPC>
2020-12-05 13:36:13 -0600

    Se creo clase de menu comidas puto Signed-off-by:jesus <jesus@RobinPC>

--- MenuComidas/src/menucomidas/MenuComidas.java
+++ MenuComidas/src/menucomidas/MenuComidas.java
@@ -1,14 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package menucomidas;
 
-/**
- *
- * @author jesus
- */
+
 public class MenuComidas {
-    
+    public static void main(String[] args) {
+        System.out.println("PUTOOOOOOOOO");
+    }
 }
